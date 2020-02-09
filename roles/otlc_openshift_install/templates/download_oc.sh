3ceafa353bfb84f7747b02aa987710d7c21eb499
Ales Nosek <ales.nosek@gmail.com>
2020-02-09 12:37:22 -0800

    Make OpenShift version configurable

--- roles/otlc_openshift_install/templates/download_oc.sh
+++ roles/otlc_openshift_install/templates/download_oc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-OCP_VERSION=4.3.0
+OCP_VERSION={{ openshift_version }}
 wget https://mirror.openshift.com/pub/openshift-v4/clients/ocp/${OCP_VERSION}/openshift-client-linux-${OCP_VERSION}.tar.gz
 tar zxvf openshift-client-linux-${OCP_VERSION}.tar.gz -C /usr/bin
 rm -f openshift-client-linux-${OCP_VERSION}.tar.gz /usr/bin/README.md

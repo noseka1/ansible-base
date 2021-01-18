68aa40196354f9b5b783403460dbd3707a8bb2d7
Ales Nosek <ales.nosek@gmail.com>
2021-01-18 14:54:33 -0800

    Refactor otlc_ocp4_installation_install to allow code reuse

--- roles/otlc_ocp4_installation_install/files/configure_aws_cli.sh
+++
@@ -1,9 +0,0 @@
-ansible localhost -m lineinfile -a 'path=$HOME/.bashrc state=present line="export REGION='"${REGION}"'"'
-
-mkdir $HOME/.aws
-cat << EOF >>  $HOME/.aws/credentials
-[default]
-aws_access_key_id = ${AWSKEY}
-aws_secret_access_key = ${AWSSECRETKEY}
-region = $REGION
-EOF

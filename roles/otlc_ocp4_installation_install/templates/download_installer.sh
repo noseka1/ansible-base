a83cbc71eb8e95425bdcdeb21a4ecf14b4eb6222
Ales Nosek <ales.nosek@gmail.com>
2021-10-03 08:46:07 -0700

    Improve parametrization of otlc_ocp4_installation_install

--- roles/otlc_ocp4_installation_install/templates/download_installer.sh
+++ roles/otlc_ocp4_installation_install/templates/download_installer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-OCP_VERSION={{ openshift_version }}
+OCP_VERSION={{ otlc_ocp4_installation_openshift_version }}
 wget https://mirror.openshift.com/pub/openshift-v4/clients/ocp/${OCP_VERSION}/openshift-install-linux-${OCP_VERSION}.tar.gz
 tar zxvf openshift-install-linux-${OCP_VERSION}.tar.gz -C /usr/bin
 rm -f openshift-install-linux-${OCP_VERSION}.tar.gz /usr/bin/README.md

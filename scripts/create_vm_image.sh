199ef59196dff7629c54421896ddd88ba241b8f9
Ales Nosek <ales.nosek@gmail.com>
2021-12-28 10:53:17 +0100

    Fix coding style of create_vm_image.sh

--- scripts/create_vm_image.sh
+++ scripts/create_vm_image.sh
@@ -1,91 +1,92 @@
 #!/bin/bash -x
-# exit immediately if a command exits with a non-zero status.
+
+# Exit immediately if a command exits with a non-zero status.
 set -e
 
-NBD_DEVICE=/dev/nbd0
+nbd_device=/dev/nbd0
 
 function mount_image {
-  local IMAGE_PATH=$1
-  local MOUNT_POINT=$2
+  local image_path=$1
+  local mount_point=$2
 
   sudo modprobe nbd
-  sudo qemu-nbd --connect $NBD_DEVICE $IMAGE_PATH
+  sudo qemu-nbd --connect $nbd_device $image_path
 
-  while [ ! -b ${NBD_DEVICE}p1 ]; do
+  while [ ! -b ${nbd_device}p1 ]; do
     # qemu-nbd can return before the block device is created by udev
     # see also: https://bugzilla.redhat.com/show_bug.cgi?id=1111635
-    echo Waiting for device ${NBD_DEVICE}p1 to show up
+    echo Waiting for device ${nbd_device}p1 to show up
     sleep 1
   done
 
-  local TABLE_TYPE=dos
-  if sudo fdisk --list $NBD_DEVICE | grep 'Disklabel type: gpt'; then
-    TABLE_TYPE=gpt
+  local table_type=dos
+  if sudo fdisk --list $nbd_device | grep 'Disklabel type: gpt'; then
+    table_type=gpt
   fi
-  local PARTITION_OFFSET
-  if [ "$TABLE_TYPE" = dos ]; then
-    PARTITION_OFFSET=$(sudo fdisk --list --units $NBD_DEVICE | grep '83 Linux' | awk '{ print $3 }')
+  local partition_offset
+  if [ "$table_type" = dos ]; then
+    partition_offset=$(sudo fdisk --list --units $nbd_device | grep '83 Linux' | awk '{ print $3 }')
   else
-    PARTITION_OFFSET=$(sudo fdisk --list --units $NBD_DEVICE 2>/dev/null | grep 'Linux filesystem' | awk '{ print $2 }')
+    partition_offset=$(sudo fdisk --list --units $nbd_device 2>/dev/null | grep 'Linux filesystem' | awk '{ print $2 }')
   fi
 
-  local PARTITION_OFFSET_BYTES=$((PARTITION_OFFSET * 512))
+  local partition_offset_bytes=$((partition_offset * 512))
 
-  echo Mounting image to $MOUNT_POINT
-  sudo mount -o loop,offset=$PARTITION_OFFSET_BYTES $NBD_DEVICE $MOUNT_POINT
+  echo Mounting image to $mount_point
+  sudo mount -o loop,offset=$partition_offset_bytes $nbd_device $mount_point
 }
 
 function umount_image {
-  local MOUNT_POINT=$1
-  echo Unmounting image from $MOUNT_POINT
-  sudo umount $MOUNT_POINT
-  rmdir $MOUNT_POINT
-  sudo qemu-nbd --disconnect $NBD_DEVICE
+  local mount_point=$1
+  echo Unmounting image from $mount_point
+  sudo umount $mount_point
+  rmdir $mount_point
+  sudo qemu-nbd --disconnect $nbd_device
 }
 
 function prepare_chroot {
-  local MOUNT_POINT=$1
-  sudo mount -t proc none $MOUNT_POINT/proc
-  sudo mount -t sysfs none $MOUNT_POINT/sys
-  sudo mount -o bind /dev $MOUNT_POINT/dev
+  local mount_point=$1
+  sudo mount -t proc none $mount_point/proc
+  sudo mount -t sysfs none $mount_point/sys
+  sudo mount -o bind /dev $mount_point/dev
 }
 
 function tear_down_chroot {
-  local MOUNT_POINT=$1
-  sudo umount $MOUNT_POINT/proc
-  sudo umount $MOUNT_POINT/sys
-  sudo umount $MOUNT_POINT/dev
+  local mount_point=$1
+  sudo umount $mount_point/proc
+  sudo umount $mount_point/sys
+  sudo umount $mount_point/dev
 }
 
-if [ -z "$IMAGE_PATH" ]; then
-  echo "IMAGE_PATH is not set. It should point to the image file to configure. Exiting ..."
+if [ -z "$image_path" ]; then
+  echo "image_path is not set. It should point to the image file to configure. Exiting ..."
   exit 1
 fi
 
-PLAYBOOK_NAME=image_vm.yml
+playbook_name=image_vm.yml
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-ANSIBLE_ROOT="$SCRIPT_DIR/.."
-MOUNT_POINT=$(mktemp -d -t mount_XXXXXX)
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ansible_root="$script_dir/.."
+mount_point=$(mktemp -d -t mount_XXXXXX)
 
-mount_image $IMAGE_PATH $MOUNT_POINT
-prepare_chroot $MOUNT_POINT
+mount_image $image_path $mount_point
+prepare_chroot $mount_point
 
 function cleanup_mount {
-  tear_down_chroot $MOUNT_POINT
-  umount_image $MOUNT_POINT
+  tear_down_chroot $mount_point
+  umount_image $mount_point
 }
 
 trap cleanup_mount EXIT
 
-ls $MOUNT_POINT/etc/cloud
+ls $mount_point/etc/cloud
 
-pushd $ANSIBLE_ROOT
+pushd $ansible_root
 sudo \
   --preserve-env \
   ansible-playbook \
     --inventory inventory/localhost.ini \
-    --extra-vars "chroot_location=$MOUNT_POINT" \
-    $PLAYBOOK_NAME \
+    --extra-vars "chroot_location=$mount_point" \
+    $playbook_name \
 || exit 1
 popd

176e5a3dca35f2c6fdaf3882d1751a91cc84d509
Ales Nosek <ales.nosek@gmail.com>
2022-07-16 12:59:34 -0700

    Install custom RPMs on OpenShift nodes

---
+++ roles/openshift_common/files/install-custom-rpms.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -x
+
+# Install additional RPM packages
+
+# `--allow-inactive` ensures that rpm-ostree does not return an error
+# if the package is already installed. This is useful if the package is
+# added to the root image in a future RH CoreOS release as it will
+# prevent the service from failing.
+
+sudo rpm-ostree install \
+  --allow-inactive \
+  --idempotent \
+  --reboot \
+  vim \
+  lsof \
+  tcpdump \
+  wireshark-cli \
+  setroubleshoot-server \
+  setools-console

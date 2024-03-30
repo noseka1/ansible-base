b02509848e33b1951b7eca61288eac7cdbd9ceee
Ales Nosek <ales.nosek@gmail.com>
2024-03-30 06:39:27 -0700

    Make LVMs PVCs work after node restart

--- roles/openshift_lvms/files/create-lvm-block.sh
+++ roles/openshift_lvms/files/create-lvm-block.sh
@@ -1,20 +1,19 @@
 #!/bin/bash -x
 
-# Create loop device
+# Create a loop block device that can be used as a storage disk
 
 device_name=loop1
 device_path=/dev/$device_name
 data_path=/var/local/${device_name}-device-data.img
 data_size=2048G
 
+# Create an empty backing file
 if [ ! -f $data_path ]; then
   truncate --size  $data_size $data_path
-  losetup --partscan --show $device_path $data_path
-else
-  losetup --partscan $device_path $data_path
 fi
 
-# Install snapshot and thin volume module for lvm
+# Initialize the loop device
+losetup --show $device_path $data_path
 
-sudo modprobe dm-snapshot
-sudo modprobe dm_thin_pool
+# This triggers LVM auto-activation as a side-effect
+losetup --set-capacity $device_path

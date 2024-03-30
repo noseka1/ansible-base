ee6a5659e39aaaf3850459a35f8b0355a48f5021
Ales Nosek <ales.nosek@gmail.com>
2024-03-30 11:42:42 -0700

    Improve block device management for OpenEBS

--- roles/openshift_openebs/files/create-openebs-lvm.sh
+++ roles/openshift_openebs/files/create-openebs-lvm.sh
@@ -1,22 +1,27 @@
 #!/bin/bash -x
 
-# Create loop device and LVM volume group
+# Create a loop device and LVM volume group
 
 device_name=loop0
 device_path=/dev/$device_name
 data_path=/var/local/${device_name}-device-data.img
 data_size=2048G
 
+# Create an empty backing file and LVM volume group
 if [ ! -f $data_path ]; then
   truncate --size  $data_size $data_path
-  losetup --partscan --show $device_path $data_path
+  losetup --show $device_path $data_path
   pvcreate $device_path
   vgcreate $device_name-vg $device_path
 else
-  losetup --partscan $device_path $data_path
+  losetup --show $device_path $data_path
 fi
 
-# Install snapshot and thin volume module for lvm
+# This triggers LVM auto-activation as a side-effect
+losetup --set-capacity $device_path
 
+# Install snapshot and thin volume module for lvm.
+# If these kernel modules are not avaialable, volume provisioning fails with:
+# thin: Required device-mapper target(s) not detected in your kernel.
 sudo modprobe dm-snapshot
 sudo modprobe dm_thin_pool

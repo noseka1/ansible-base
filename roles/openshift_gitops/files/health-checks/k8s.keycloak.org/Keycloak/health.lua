a52f6c879121bbda9bf6485f9b67a3f485371878
Ales Nosek <ales.nosek@gmail.com>
2024-04-23 20:08:32 -0700

    Update Keycloak version to 22.0.10

---
+++ roles/openshift_gitops/files/health-checks/k8s.keycloak.org/Keycloak/health.lua
@@ -0,0 +1,33 @@
+health_status = {}
+if obj.status ~= nil then
+  if obj.status.conditions ~= nil then
+    numDegraded = 0
+    numProgressing = 0
+    msg = ""
+    for i, condition in pairs(obj.status.conditions) do
+      msg = msg .. i .. ": " .. condition.type .. " = " .. condition.status .. "\n"
+      if condition.type == "HasErrors" and condition.status == "True" then
+        numDegraded = numDegraded + 1
+      elseif (condition.type == "RollingUpdate" and condition.status == "True") or
+          (condition.type == "Ready" and condition.status == "False") then
+        numProgressing = numProgressing + 1
+      end
+    end
+    if numProgressing > 0 then
+      health_status.status = "Progressing"
+      health_status.message = msg
+      return health_status
+    elseif numDegraded == 0 then
+      health_status.status = "Healthy"
+      health_status.message = msg
+      return health_status
+    else
+      health_status.status = "Degraded"
+      health_status.message = msg
+      return health_status
+    end
+  end
+end
+health_status.status = "Progressing"
+health_status.message = "Reconciliation in progress"
+return health_status
--- roles/openshift_rhbk/README.md
+++ roles/openshift_rhbk/README.md
@@ -1,8 +1,8 @@
-# Ansible role for deploying Keycloak (RHSSO) to OpenShift
+# Ansible role for deploying Red Hat build of Keycloak to OpenShift
 
 ## References
 
-* [Product Documentation for Red Hat Single Sign-On 7.6](https://access.redhat.com/documentation/en-us/red_hat_single_sign-on/7.6)
+* [Product Documentation for Red Hat build of Keycloak](https://access.redhat.com/documentation/en-us/red_hat_build_of_keycloak)
 * [Red Hat Single Sign-On for OpenShift](https://access.redhat.com/documentation/en-us/red_hat_single_sign-on/7.6/html/red_hat_single_sign-on_for_openshift/index)
 * [Keycloak operator](https://www.keycloak.org/guides#operator)
 * [Keycloak on Quarkus](https://github.com/keycloak/keycloak/tree/main/operator)

a12545e2190c70f964d5f15b18e01076c8e4c6d1
Ales Nosek <ales.nosek@gmail.com>
2025-03-08 08:45:58 -0800

    Convert openshift_cryostat to autogitops v2

---
+++ roles/openshift_gitops/files/health-checks/operator.cryostat.io/Cryostat/health.lua
@@ -0,0 +1,24 @@
+health_status = {}
+if obj.status ~= nil then
+  if obj.status.conditions ~= nil then
+    numAvailable = 0
+    msg = ""
+    for i, condition in pairs(obj.status.conditions) do
+      msg = msg .. i .. ": " .. condition.type .. " = " .. condition.status .. "\n"
+      if condition.type == "MainDeploymentAvailable" and condition.status == "True" then
+        numAvailable = numAvailable + 1
+      end
+      if condition.type == "ReportsDeploymentAvailable" and condition.status == "True" then
+        numAvailable = numAvailable + 1
+      end
+    end
+    if numAvailable > 1 then
+      health_status.status = "Healthy"
+      health_status.message = msg
+      return health_status
+    end
+  end
+end
+health_status.status = "Progressing"
+health_status.message = "Reconciliation in progress"
+return health_status

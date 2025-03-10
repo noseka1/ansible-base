3d689c96d6e1b3cb6761e9eae8cdca6f5dfe93fe
Ales Nosek <ales.nosek@gmail.com>
2025-03-10 11:52:21 -0700

    Fix errors logged by create-lvms-block.sh

--- roles/openshift_lvms/files/create-lvms-block.sh
+++ roles/openshift_lvms/files/create-lvms-block.sh
@@ -4,16 +4,18 @@
 
 device_name=loop1
 device_path=/dev/$device_name
-data_path=/var/local/${device_name}-device-data.img
+data_path=/var/local/lvms-${device_name}-device-data.img
 data_size=2048G
 
 # Create an empty backing file
 if [ ! -f $data_path ]; then
-  truncate --size $data_size $data_path
+  truncate \
+    --size $data_size \
+    $data_path
 fi
 
 # Initialize the loop device
-losetup --show $device_path $data_path
-
-# This triggers LVM auto-activation as a side-effect
-losetup --set-capacity $device_path
+losetup \
+  --show \
+  $device_path \
+  $data_path
